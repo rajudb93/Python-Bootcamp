239c6cca2567dddcae2ec6de7e402350bf27812d
Raju Dubey <rajudb93@gmail.com>
2025-08-09 16:40:24 +0530

    swapping the temperature

---
+++ 00_python/SwapTemp.py
@@ -0,0 +1,10 @@
+# Swapping min_temp and max_temp using tuple unpacking
+
+min_temp = 40
+max_temp = 25
+
+# Swap the values
+min_temp, max_temp = max_temp, min_temp
+
+print("Correct min_temp:", min_temp)
+print("Correct max_temp:", max_temp)
