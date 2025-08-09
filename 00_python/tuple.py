53d35f30ebbf75f1117388930dd05467388920fe
Raju Dubey <rajudb93@gmail.com>
2025-08-09 16:33:26 +0530

    added few extra operation on tuple

--- 00_python/tuple.py
+++ 00_python/tuple.py
@@ -1,14 +1,43 @@
-masala=("paper","turmeric","chili powder","cumin","coriander","garam masala"
-        ,"cinnamon","cardamom","cloves","bay leaf")
 
-print("The first spice is:", masala[0])
-print("The last spice is:", masala[-1])
-print("The number of spices in the list is:", len(masala))
-print("The spices in the list are:")
+
+# Tuple creation
+masala = ("paper", "turmeric", "chili powder", "cumin", "coriander",
+          "garam masala", "cinnamon", "cardamom", "cloves", "bay leaf")
+
+# Accessing elements
+print("First spice:", masala[0])
+print("Last spice:", masala[-1])
+
+# Tuple length
+print("Total spices:", len(masala))
+
+# Iterating through tuple
+print("All spices:")
 for spice in masala:
     print(spice)
-    
 
-# Simple Python Starter Script
-print(f"\n Is ginger in the masala list? {'ginger' in masala}")
+# Slicing a tuple (first 3 spices)
+print("First three spices:", masala[:3])
+
+# Membership test
+if "ginger" in masala:
+    print("Ginger is in the masala tuple.")
+else:
+    print("Ginger is NOT in the masala tuple.")
+
+# Tuple concatenation
+extra = ("ginger", "mustard")
+all_spices = masala + extra
+print("All spices after adding more:", all_spices)
+
+# Tuple repetition
+repeat = ("salt",) * 3
+print("Tuple with repeated salt:", repeat)
+
+# Unpacking tuple
+first, second, *rest = masala
+print("First spice:", first)
+print("Second spice:", second)
+print("Rest of the spices:", rest)
+
 
