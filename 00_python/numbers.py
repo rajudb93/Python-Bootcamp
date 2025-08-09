ac02a5ad56ed9b188d8d797665a8f7dacfad2d6c
Raju Dubey <rajudb93@gmail.com>
2025-08-09 16:13:46 +0530

    Add tuple example for spices with indexing, length, iteration

---
+++ 00_python/numbers.py
@@ -0,0 +1,6 @@
+#find the sum of two numbers
+
+no_of_flowers=5
+no_of_fruits=10
+total_items = no_of_flowers + no_of_fruits
+print(f"Total items: {total_items}")
---
+++ 00_python/tuple.py
@@ -0,0 +1,14 @@
+masala=("paper","turmeric","chili powder","cumin","coriander","garam masala"
+        ,"cinnamon","cardamom","cloves","bay leaf")
+
+print("The first spice is:", masala[0])
+print("The last spice is:", masala[-1])
+print("The number of spices in the list is:", len(masala))
+print("The spices in the list are:")
+for spice in masala:
+    print(spice)
+    
+
+# Simple Python Starter Script
+print(f"\n Is ginger in the masala list? {'ginger' in masala}")
+
