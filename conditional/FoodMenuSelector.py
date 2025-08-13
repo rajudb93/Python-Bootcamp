2bf0315a478c7853fec9744e9688efd5780d8a7b
Raju Dubey <rajudb93@gmail.com>
2025-08-13 07:19:53 +0530

    added example program for conditional statement

---
+++ conditional/FoodMenuSelector.py
@@ -0,0 +1,15 @@
+
+# Food Menu Selector using match-case (Python 3.10+)
+item = input("Enter food item: ").lower()
+
+match item:
+    case "pizza":
+        print("Price: 30 bucks")
+    case "burger":
+        print("Price: 15 bucks")
+    case "pasta":
+        print("Price: 20 bucks")
+    case "salad":
+        print("Price: 10 bucks")
+    case _:
+        print("Item not available")
---
+++ conditional/Input.py
@@ -0,0 +1,6 @@
+snacks=input("Enter you favorire snacks").lower()
+
+if snacks=="samosa" or snacks=="chips":
+    print("Entered choice is:", snacks)
+else:
+    print("Entered choice is not samosa or chips")
